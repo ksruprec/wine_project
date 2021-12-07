d4e21fcba1969fc6fc47e6e82b913f6308b5d621
Kristen Ruprecht <ksruprec@gmail.com>
2021-12-07 10:48:53 -0500

    converted to django-rest-framework

--- wine_api/settings.py
+++ wine_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'wines',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- wines/urls.py
+++ wines/urls.py
@@ -1,10 +1,25 @@
 from django.urls import path
-from . import views
+from rest_framework import urlpatterns
+from .views import WineViewSet
+from rest_framework.routers import DefaultRouter
+# from . import views
 
-urlpatterns = [
-    path('', views.wine_list, name='wine_list'),
-    path('new', views.new_wine, name='new_wine'),
-    path('<int:wine_id>', views.wine_detail, name='wine_detail'),
-    path('<int:wine_id>/edit', views.edit_wine, name='edit_wine'),
-    path('<int:wine_id>/delete', views.delete_wine, name='delete_wine'),
-]
+router = DefaultRouter()
+router.register(r'', WineViewSet, basename='wines')
+urlpatterns = router.urls
+
+for url in urlpatterns:
+    print(url)
+
+# urlpatterns = [
+#     path('', views.wine_list, name='wine_list'),
+#     path('new', views.new_wine, name='new_wine'),
+#     path('<int:wine_id>', views.wine_detail, name='wine_detail'),
+#     path('<int:wine_id>/edit', views.edit_wine, name='edit_wine'),
+#     path('<int:wine_id>/delete', views.delete_wine, name='delete_wine'),
+# ]
+
+# 1. pip install djangorestframework
+# 2. settings.py (add to app: rest_framework)
+# 3. App/URLS.py ( from .views import WineViewSet )
+# 4. App/URLS.py ( from rest_framework import DefaultRouter )
